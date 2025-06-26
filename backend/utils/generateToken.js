2a830ff5f7ee37d7bb7f2ea7ec499b0121910ae3
Bhavya Sri <annabathulabhavyasri@gmail.com>
2025-06-27 01:19:14 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 import { ENV_VARS } from "../config/envVars.js";
 
 export const generateTokenAndSetCookie = (userId, res) => {
+  // JWT token is created with User-Id
   const token = jwt.sign({ id: userId }, ENV_VARS.JWT_SECRET, { 
     expiresIn: "15d" 
   });
